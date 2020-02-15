13ea4c1f14a32e399fbad25f49e9b3c18843a1f0
Ankush <linktoankush@gmail.com>
2020-02-15 18:21:58 +0100

    Soulution

--- HackerEarth_MinimumSteps/HackerEarth_MinimumSteps.cpp
+++ HackerEarth_MinimumSteps/HackerEarth_MinimumSteps.cpp
@@ -1,20 +1,68 @@
 // HackerEarth_MinimumSteps.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
+
+bool myfunction(pair<int, int>& A1, pair<int, int>& A2)
+{
+    return A1.first < A2.first;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    int N;
+    std::vector<pair<int, int>> arrPos;
+
+    std::cin >> N;
+
+    for (int i = 0; i < N; ++i)
+    {
+        int tmp;
+        std::cin >> tmp;
+        arrPos.push_back( make_pair(tmp, i));
+        //Arr2.push_back(tmp);
+    }
+
+    sort(arrPos.begin(), arrPos.end(), myfunction);
+
+    // To keep track of visited elements. Initialize 
+    // all elements as not visited or false. 
+    vector<bool> vis(N, false);
+
+    // Initialize result 
+    int ans = 0;
+
+    // Traverse array elements 
+    for (int i = 0; i < N; i++)
+    {
+        // already swapped and corrected or 
+        // already present at correct pos 
+        if (vis[i] || arrPos[i].second == i)
+            continue;
+
+        // find out the number of  node in 
+        // this cycle and add in ans 
+        int cycle_size = 0;
+        int j = i;
+        while (!vis[j])
+        {
+            vis[j] = 1;
+
+            // move to next node 
+            j = arrPos[j].second;
+            cycle_size++;
+        }
+
+        // Update answer by adding current cycle.  
+        if (cycle_size > 0)
+        {
+            ans += (cycle_size - 1);
+        }
+    }
+    //std::cout << std::endl;
+    std::cout << ans << std::endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
